066f0e0116dfc2f914e97db5ebdf77856e99b694
Jacky Mong Marma <93147721+Jackymong@users.noreply.github.com>
2022-08-23 10:02:51 +0600

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -1,7 +1,29 @@
+#Polymorphism ( Method Overriding )
+class Phone:
+    def __init__(self,price,brand,cemera):
+        print("Inside phone constructor")
+        self.__price = price
+        self.__brand = brand
+        self.__camera = cemera
+
+    def buy(self):
+        print("Buying phone")
+
+    def return_phone(self):
+        print("Return phone")
+
+class FeaturePhone(Phone):
+    pass
+
+class SmartPhone(Phone):
+    def buy(self):
+        print("Buying a Smartphone")
+
+s = SmartPhone(12000,"Apple",13)
+s.buy()
 
 
-
-##Polymorphism (Method Overloading)
+#Polymorphism (Method Overloading)
 class Geometry:
 
     def area(self,a,b=0):
@@ -11,4 +33,4 @@ class Geometry:
             print("Rect",a*b)
 obj = Geometry()
 obj.area(3)
-obj.area(3,2)
+obj.area(3,2)  
